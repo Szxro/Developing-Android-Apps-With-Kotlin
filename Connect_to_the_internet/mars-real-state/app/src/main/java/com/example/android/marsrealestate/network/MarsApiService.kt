0705f02fa03a6ba3fb06987d6d57282853096e2a
Szxro <szxro@gmail.com>
2025-09-26 11:24:25 -0400

    Finish Mars Project

---
+++ Connect_to_the_internet/mars-real-state/app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.example.android.marsrealestate.network
+
+import retrofit2.create
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+enum class MarsApiFilter(val value: String) { SHOW_RENT("rent"), SHOW_BUY("buy"), SHOW_ALL("all") }
+
+enum class MarsApiStatus { LOADING, ERROR, DONE }
+
+interface MarsApiService {
+    @GET("realestate")
+    suspend fun getProperties(@Query("filter") type: String): List<MarsProperty>
+}
+
+object MarsApi {
+    val retrofitService : MarsApiService by lazy {
+        RetrofitClient.getInstance()!!.create<MarsApiService>();
+    }
+}
